32b5cd405db8e250d6ad5202a549bec3a4561807
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-02-25 13:53:34 +0700

    Push shellScript

--- Desktop/Intern/shellScript/bai3.sh
+++ Desktop/Intern/shellScript/bai3.sh
@@ -1,1 +1,9 @@
-git
+#!/bin/bash 
+DIR="/Users/admin/Desktop/Intern/"
+if [ -d "$DIR" ]; then
+echo "Thư mục $DIR tồn tại. Gồm: "
+ls "$DIR"
+else 
+echo "Thư mục $DIR không tồn tại tại"
+exit 1;
+fi
