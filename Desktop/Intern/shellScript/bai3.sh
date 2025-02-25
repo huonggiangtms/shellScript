106700421c654c512689c6f353203ae0f038a53d
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-02-25 14:17:07 +0700

    push again

--- Desktop/Intern/shellScript/bai3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-DIR="/Users/admin/Desktop/Intern/"
-if [ -d "$DIR" ]; then
-echo "Thư mục $DIR tồn tại. Gồm: "
-ls "$DIR"
-else 
-echo "Thư mục $DIR không tồn tại tại"
-exit 1;
-fi
--- Desktop/Intern/shellScript/bai4.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-BACKUP_DIR="/Users/admin/Desktop/Intern/shellScript_backup_$(date +%Y%m%d_%H%M%S)"
-cp -r /Users/admin/Desktop/Intern/shellScript "$BACKUP_DIR"
-log_info "Backup thư mục shellScript vào $BACKUP_DIR"
